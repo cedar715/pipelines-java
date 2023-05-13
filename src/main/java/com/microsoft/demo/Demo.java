364658594f2ec62bab8751d2ee764b3d093dbad2
Jai V <jaya.prakash@technofacts.com>
2023-05-13 16:40:46 +0530

    NO-JIRA minor refactoring

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,12 +1,12 @@
 package com.microsoft.demo;
 
 public class Demo {
-    public void DoSomething(boolean flag){
+    public void doSomething(boolean flag){
         if(flag){
             System.out.println("I am covered");
-            return;
-        }
+        } else {
 
-        System.out.println("I am not covered");
+            System.out.println("I am not covered");
+        }
     }
 }
